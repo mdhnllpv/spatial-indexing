7b35bbcc9301e382718d0b64bf06af2a85269a18
manigoad <manigoad@92272e72-444f-11de-9a54-b9394989f659>
2009-05-19 08:48:39 +0000

    Initial version

---
+++ src/gui/Start.java
@@ -0,0 +1,13 @@
+package gui;
+
+public class Start {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MainApplication application = new MainApplication();
+		application.setVisible(true);
+	}
+
+}
