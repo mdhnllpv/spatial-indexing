dfa66f176c6cfa8a0ac551c56ab6b842817f30a0
manigoad <manigoad@92272e72-444f-11de-9a54-b9394989f659>
2009-05-19 08:48:39 +0000

    Initial version

---
+++ trunk/src/gui/MainApplication.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class MainApplication extends JFrame{
+	
+	private static final long serialVersionUID = 1800617154369993903L;
+
+	public MainApplication(){
+		setSize(300, 400);
+		setTitle("Spatial Indexing");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+	}
+
+}
